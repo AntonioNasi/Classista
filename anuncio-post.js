bdb0aa8157f434fa83dead42d45187fbfdcc782c
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2021-10-03 20:07:33 -0300

    1.4 Banner2 and Anuncio Post changes

---
+++ anuncio-post.js
@@ -0,0 +1,8 @@
+// Configuração dos anuncios no meio dos posts
+
+var anuncio = window.document.getElementById('anu-post')
+
+anuncio.innerHTML = ('<a href="https://coletivoveredas.com" target="_blank"> <h4 style="padding:0px; margin:0px; color:red; font-weight:900"> Coletivo Veredas </h4> <p style="padding:0px; margin:0px; color:#000000;" > Acesse o site do Coletivo Veredas e compre livros por preço de custo! </p> <p style="padding:0px; margin:0px; color:blue;" > Clique aqui </p> </a>')
+
+anuncio.style.border = 'solid 1px #000000'
+anuncio.style.padding = '10px'
