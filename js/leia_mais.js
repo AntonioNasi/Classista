0c811c30831f8fbb27a8cbf5fef5d019936700b1
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2022-07-02 17:35:20 -0300

    Add files via upload

--- js/leia_mais.js
+++ js/leia_mais.js
@@ -1,3 +1,21 @@
+function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Continuar lendo"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Minimizar leitura"; 
+        moreText.style.display = "inline";
+    }
+}
+
+
+
 function myFunction2() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
