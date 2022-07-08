efb6dec3e1bdd863cbd29a64a5081e66caedcff2
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2022-07-08 14:00:20 -0300

    2.0 new(old) posts

--- leia_mais.js
+++ leia_mais.js
@@ -13,3 +13,37 @@ function myFunction1() {
         moreText.style.display = "inline";
     }
 }
+
+
+function myFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("bt2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Continuar lendo"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Minimizar leitura"; 
+        moreText.style.display = "inline";
+    }
+}
+
+
+function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("bt3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Continuar lendo"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Minimizar leitura"; 
+        moreText.style.display = "inline";
+    }
+}
