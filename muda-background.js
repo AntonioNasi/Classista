9494b4573d1784e9baa5b0443b7351f7f796379d
AntonioNasi <86308977+AntonioNasi@users.noreply.github.com>
2022-08-19 11:33:55 -0300

    Add files via upload

---
+++ muda-background.js
@@ -0,0 +1,9 @@
+function changeFundo(){
+    var changeF = window.document.getElementById('changeFundo')
+    changeF.style.backgroundImage = "url('./bannerestatico1.svg')"
+}
+
+function voltaFundo(){
+    var voltaF = window.document.getElementById('changeFundo')
+    voltaF.style.backgroundImage = "url('./bannerestatico.svg')"
+}
